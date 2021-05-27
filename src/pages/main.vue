b5ccb332a744b5dbead15ca2d71d6c5cc445bc80
ThitipongF <84861299+ThitipongF@users.noreply.github.com>
2021-05-27 16:57:45 +0700

    Initial commit

---
+++ src/pages/main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <manufacturer v-slot="data">
+      <devices v-bind:manufacturerId="data.factId"></devices>
+    </manufacturer>
+  </div>
+</template>
+
+<script>
+import Manufacturer from "@/components/Manufacturer.vue";
+import Devices from "@/components/Devices.vue";
+export default {
+  components: {
+    Manufacturer: Manufacturer,
+    Devices: Devices,
+  },
+};
+</script>
+
+
